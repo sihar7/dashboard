32392894ed31743be0c9ad6cd03f981b390d1a84
Sigit Hardianto <muhammadsigit.hd20@gmail.com>
2021-07-27 14:44:19 +0700

    perbaikan dashboard

--- resources/views/management/dashboard.blade.php
+++ resources/views/management/dashboard.blade.php
@@ -26,40 +26,53 @@ body{width:100%;
 
 <div class="row">
     <div class="col-xl-3 col-sm-6">
-        <div class="card" style="height:582px;">
+        <div class="card" style="height:852px;">
             <div class="card-body">
                 <center><h4 class="card-title">Telemarketing</h4></center>
-
+                <br><br>
                 <div class="">
-                    <div class="alert alert-success bg-success text-white" role="alert">
+                    <div class="alert alert-success bg-success text-white" role="alert" style="height:79px;">
                         <h4>Bryan 1</h4>
                         <p>Online</p>
                     </div>
-                    <div class="alert alert-success bg-success text-white" role="alert">
+                    <div class="alert alert-success bg-success text-white" role="alert" style="height:79px;">
                         <h4>Bryan 2</h4>
                         <p>Online</p>
                     </div>
-                    <div class="alert alert-success bg-success text-white" role="alert">
+                    <div class="alert alert-success bg-success text-white" role="alert" style="height:79px;">
                         <h4>Bryan 3</h4>
                         <p>Online</p>
                     </div>
 
-                    <div class="alert alert-success bg-danger text-white" role="alert">
+                    <div class="alert alert-success bg-danger text-white" role="alert" style="height:79px;">
                         <h4>Bryan 4</h4>
                         <p>Active 2 Days Ago</p>
                     </div>
+                    <div class="alert alert-success bg-danger text-white" role="alert" style="height:79px;">
+                        <h4>Bryan 5</h4>
+                        <p>Active 2 Days Ago</p>
+                    </div>
+                    <div class="alert alert-success bg-danger text-white" role="alert" style="height:79px;">
+                        <h4>Bryan 6</h4>
+                        <p>Active 2 Days Ago</p>
+                    </div>
+                     <div class="alert alert-success bg-danger text-white" role="alert" style="height:79px;">
+                        <h4>Bryan 7</h4>
+                        <p>Active 2 Days Ago</p>
+                    </div>
 
-                    <br><br><br>
                 </div>
             </div>
         </div>
     </div>
     <!-- end col -->
     <div class="col-xl-6 col-sm-6">
-        <div class="card">
+        <div class="card" style="height:852px;">
             <div class="card-body">
+
                 <center><h2 class="card-title">TOP 10</h2></center>
                 <center>
+                    <br><br>
                     <div class="row">
                         <div class="col-md-6">
                             <div class="col-md-6">
@@ -82,6 +95,8 @@ body{width:100%;
 
                 </center>
 
+                <br>
+                <br>
                 <br>
                 <br>
                 <div id="carouselExampleCaption" class="carousel slide" data-bs-ride="carousel">
@@ -135,18 +150,20 @@ body{width:100%;
         </div>
     </div>
     <div class="col-xl-3 col-sm-6">
-        <div class="card">
+        <div class="card" style="height:852px;">
             <br>
             <center>
                 <h4 class="card-title">Hello !</h4></center>
                 <br>
                 <center><img class="rounded-circle mt-4 mt-sm-0" alt="200x200" width="260" height="260" src="{{ asset('assets/images/users/user-4.jpg') }}" data-holder-rendered="true"></center>
                <div class="card-body">
-                   <center>
+                <br><br>
+                <center>
                 <p class="card-text">Congrats Atas Pencapaiannya !</p>
                 <h2 class="card-text">BRYAN ARDYAWAN</h2>
                 <br>
                 <br>
+                <br><br><br><br>
                 <h4 class="card-text">Pendapatan Polis</h4>
                 <h2 class="card-text">RP 1,432.000</h2>
                 </center>
@@ -167,32 +184,31 @@ body{width:100%;
 
                 <center>
                 <div class="row">
-
                     <div class="col-lg-4">
-                        <div class="card text-white bg-info">
+                        <div class="card text-white bg-info" style="width:185px; height:178.34px; background: linear-gradient(45deg, #FF00C7, #020202);">
                             <div class="card-body">
                                 <p>Daily</p>
-                                <button type="button" class="btn btn-outline-light waves-effect">Detail</button>
+                                <button type="button" class="btn btn-outline-light waves-effect" style="color:#fff; border-color:#fff;">Detail</button>
                                 <i class="ion ion-md-download" style="width:35px; height:35px;"></i>
                             </div>
                         </div>
                     </div>
 
                     <div class="col-lg-4">
-                        <div class="card text-white bg-warning">
+                        <div class="card text-white bg-warning" style="width:185px; height:178.34px; background: linear-gradient(45deg, #0049FF, #020202);">
                             <div class="card-body">
                                 <p>Weekly</p>
-                                <button type="button" class="btn btn-outline-light waves-effect">Detail</button>
+                                <button type="button" class="btn btn-outline-light waves-effect" style="color:#fff; border-color:#fff;">Detail</button>
                                 <i class="ion ion-md-download"></i>
                             </div>
                         </div>
                     </div>
 
                     <div class="col-lg-4">
-                        <div class="card text-white bg-danger">
+                        <div class="card text-white bg-danger"  style="width:185px; height:178.34px; background: linear-gradient(45deg, #FF0037, #020202);">
                             <div class="card-body">
                                 <p>Monthly</p>
-                                <button type="button" class="btn btn-outline-light waves-effect">Detail</button>
+                                <button type="button" class="btn btn-outline-light waves-effect" style="color:#fff; border-color:#fff;">Detail</button>
                                 <i class="ion ion-md-download"></i>
                             </div>
                         </div>
